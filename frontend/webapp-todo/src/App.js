b8309ea5e5aaa303a53be768f5863b802cd9879b
Facundo Palombo <facundo.palombo@gmail.com>
2020-04-28 01:16:10 -0300

    Fixing docker-compose and Dockerfiles

--- frontend/webapp-todo/src/App.js
+++ frontend/webapp-todo/src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { Link, Switch, BrowserRouter, Route} from 'react-router-dom'
+import { IoIosAddCircle } from 'react-icons/io'
+
+const initialState = {}
+const store = createStore(() => initialState)
 
 function App() {
+  const micomponente = () => (<article> this is an article and THIS IS A <Link to='/hello'>REDIRECT</Link></article>)
+  const micomp2 = () => (<> <IoIosAddCircle size="50px"/> Hello world in react router</>)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloaddddd.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+            <Route path='/hello' component={micomp2}/>
+            <Route path='/' component={micomponente} />
+          </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
